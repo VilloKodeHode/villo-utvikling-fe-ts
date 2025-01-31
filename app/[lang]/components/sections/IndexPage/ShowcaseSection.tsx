d0eb9f1799ef62712ad569e9fce927eff34d631b
Joakim <villokodehode@gmail.com>
2025-01-31 16:51:41 +0100

    pre tailwind migration (from home)

--- app/[lang]/components/sections/IndexPage/ShowcaseSection.tsx
+++ app/[lang]/components/sections/IndexPage/ShowcaseSection.tsx
@@ -29,51 +29,52 @@ export const ShowcaseSection = ({ dictionary }) => {
   return (
     <>
       <section
-        className="relative animate-on-scroll opacity-0 z-10 flex items-center justify-center w-full h-[100vh] group/imageEffect"
+        className="relative animate-on-scroll opacity-0 z-10 flex items-center justify-center w-full group/imageEffect"
         id="showcase-section"
       >
-        <div className="grid place-content-center h-full m-0">
-          <div className="grid sm:grid-cols-2 h-[100vh] items-center gap-8 w-full max-w-6xl grid-cols-1 sm:grid-rows-1 grid-rows-3">
-            <div className="w-full relative z-10 select-none sm:self-center self-end">
-              <ThemedH2 className="flex sm:block justify-center gap-x-3 md:gap-x-0">
-                {content.map((showCase, index) => (
-                  <Showcase
-                    key={showCase.alt + index}
-                    index={index}
-                    activeImage={activeImageIndex}
-                    setActiveImage={setActiveImageIndex}
-                    content={content}
-                  />
-                ))}
-              </ThemedH2>
+        {/* <div className="grid sm:grid-cols-2 h-[100vh] items-center gap-8 w-full max-w-6xl grid-cols-1 sm:grid-rows-1 grid-rows-3"></div> */}
+          <div className="flex sm:flex-row h-[100vh] items-center gap-8 w-full max-w-6xl flex-col">
+            <div className="w-full relative z-10 select-none sm:self-center self-end flex sm:block justify-center gap-x-3 md:gap-x-0">
+              {content.map((showCase, index) => (
+                <Showcase
+                  key={showCase.alt + index}
+                  index={index}
+                  activeImage={activeImageIndex}
+                  setActiveImage={setActiveImageIndex}
+                  content={content}
+                />
+              ))}
+  
               <ThemedP className="mb-8 hidden h-[72px] sm:block">
-                
                 {content[activeImageIndex].paragraph}
               </ThemedP>
             </div>
 
-              <div
-                className={`justify-self-end z-10 grid items-center justify-center w-full transition-all relative md:w-full h-full sm:h-[450px] outline-offset-4 rounded-2xl overflow-hidden outline-2 outline outline-Villo-light-primary dark:outline-Villo-dark-primary
+            <div
+              className={`justify-self-end z-10 grid items-center justify-center w-full transition-all relative md:w-full h-full sm:h-[450px] outline-offset-4 rounded-2xl overflow-hidden outline-2 outline outline-Villo-light-primary dark:outline-Villo-dark-primary
                   `}
-              >
-                {content.map((image, index) => (
-                  <Image
-                    key={image.alt}
-                    src={image.src}
-                    alt={image.alt}
-                    width={700}
-                    height={700}
-                    className={`${image.fileType === "svg" ? "object-contain bg-Villo-dark-white20 p-8" : "object-cover object-left-top"} absolute duration-500  transition-all rounded-[4px] w-full h-full ${
-                      index === activeImageIndex ? "opacity-100" : "opacity-0"
-                    } `}
-                  />
-                ))}
-              </div>
-              <ThemedP className="max-h-[48px] mb-8 block self-start sm:hidden">
-                {content[activeImageIndex].paragraph}
-              </ThemedP>
+            >
+              {content.map((image, index) => (
+                <Image
+                  key={image.alt}
+                  src={image.src}
+                  alt={image.alt}
+                  width={700}
+                  height={700}
+                  className={`${
+                    image.fileType === "svg"
+                      ? "object-contain bg-Villo-dark-white20 p-8"
+                      : "object-cover object-left-top"
+                  } absolute duration-500  transition-all rounded-[4px] w-full h-full ${
+                    index === activeImageIndex ? "opacity-100" : "opacity-0"
+                  } `}
+                />
+              ))}
+            </div>
+            <ThemedP className="max-h-[48px] mb-8 block self-start sm:hidden">
+              {content[activeImageIndex].paragraph}
+            </ThemedP>
           </div>
-        </div>
       </section>
     </>
   );
@@ -84,25 +85,27 @@ export const Showcase = ({ content, setActiveImage, activeImage, index }) => {
     return null;
   } else {
     return (
-      <span
-        onMouseEnter={() => setActiveImage(index)}
-        className={`cursor-pointer group sm:block flex sm:flex-row flex-col items-center align-middle border
+      <ThemedH2 className="">
+        <span
+          onMouseEnter={() => setActiveImage(index)}
+          className={`cursor-pointer group sm:block flex sm:flex-row flex-col items-center align-middle border
             text-Villo-light-primary
             dark:text-Villo-dark-primary`}
-      >
-        <span
-          className={`
+        >
+          <span
+            className={`
               group-hover:text-Villo-light-primary dark:group-hover:text-Villo-dark-primary group-hover:opacity-100 transition-all duration-500 opacity-60 h-fit h1hidden ${
                 activeImage === index
                   ? "text-Villo-dark-primary"
                   : "text-Villo-light-white20 dark:text-Villo-dark-black50"
               }`}
-        >
-          <br />
-          Web
+          >
+            <br />
+            Web
+          </span>
+          {content[index].type}
         </span>
-        {content[index].type}
-      </span>
+      </ThemedH2>
     );
   }
 };
