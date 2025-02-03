5076677cc974182bf8d232bd1d52a06bb58a3a81
Joakim-Jobloop <Joakim@jobloop.no>
2025-02-03 15:25:11 +0100

    *

--- app/[lang]/components/atoms/ThemedText.tsx
+++ app/[lang]/components/atoms/ThemedText.tsx
@@ -1,40 +1,61 @@
 interface ThemedTextProps {
-    children: React.ReactNode;
-    className?: string;
+  children: React.ReactNode;
+  className?: string;
 }
 
 export const ThemedH1 = ({ children, className }: ThemedTextProps) => {
   return (
-      <h1 className={`text-light-black
-       dark:text-dark-white md:text-5xl text-4xl ${className}`}>
-          {children}
-      </h1>
-  )
-}
-
+    <h1
+      className={`text-light-black
+       dark:text-dark-white md:text-5xl text-4xl ${className}`}
+    >
+      {children}
+    </h1>
+  );
+};
 
 export const ThemedP = ({ children, className }: ThemedTextProps) => {
-    return (
-        <p className={`text-light-black
-         dark:text-dark-white ${className}`}>
-            {children}
-        </p>
-    )
-}
+  return (
+    <p
+      className={`text-light-black
+         dark:text-dark-white ${className}`}
+    >
+      {children}
+    </p>
+  );
+};
 
 export const ThemedH2 = ({ children, className }: ThemedTextProps) => {
-    return (
-      <h2 className={`text-light-black
-       dark:text-dark-white md:text-4xl sm:text-3xl text-2xl ${className}`}>
-        {children}
-      </h2>
-    );
-  };
+  return (
+    <h2
+      className={`text-light-black
+       dark:text-dark-white sm:text-h2 text-h4 leading-h4 sm:leading-h2  ${className}`}
+    >
+      {children}
+    </h2>
+  );
+};
+
+export const ShowCaseHeaderText = ({
+  children,
+  className,
+}: ThemedTextProps) => {
+  return (
+    <h2
+      className={`text-light-black
+       dark:text-dark-white sm:text-h2 ml:text-h4 text-h5 ml:leading-h4 leading-h5 sm:leading-h2  ${className}`}
+    >
+      {children}
+    </h2>
+  );
+};
 
 export const ThemedH4 = ({ children, className }: ThemedTextProps) => {
   return (
-    <h4 className={`text-light-black
-     dark:text-dark-white md:text-xl text-lg ${className}`}>
+    <h4
+      className={`text-light-black
+     dark:text-dark-white md:text-xl text-lg ${className}`}
+    >
       {children}
     </h4>
   );
@@ -42,10 +63,11 @@ export const ThemedH4 = ({ children, className }: ThemedTextProps) => {
 
 export const ThemedH5 = ({ children, className }: ThemedTextProps) => {
   return (
-    <h5 className={`text-light-black
-     dark:text-dark-white md:text-lg text-md ${className}`}>
+    <h5
+      className={`text-light-black
+     dark:text-dark-white md:text-lg text-md ${className}`}
+    >
       {children}
     </h5>
   );
 };
-
--- app/[lang]/components/sections/IndexPage/ShowcaseSection.tsx
+++ app/[lang]/components/sections/IndexPage/ShowcaseSection.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import { useEffect, useState } from "react";
-import { ThemedH2, ThemedP } from "@components/atoms/ThemedText";
+import { ShowCaseHeaderText, ThemedP } from "@components/atoms/ThemedText";
 
 export const ShowcaseSection = ({ dictionary }) => {
   const content = dictionary.showCaseList;
@@ -33,48 +33,50 @@ export const ShowcaseSection = ({ dictionary }) => {
         id="showcase-section"
       >
         {/* <div className="grid sm:grid-cols-2 h-[100vh] items-center gap-8 w-full max-w-6xl grid-cols-1 sm:grid-rows-1 grid-rows-3"></div> */}
-          <div className="flex sm:flex-row h-[100vh] items-center gap-8 w-full max-w-6xl flex-col">
-            <div className="w-full relative z-10 select-none sm:self-center self-end flex sm:block justify-center gap-x-3 md:gap-x-0">
-              {content.map((showCase, index) => (
-                <Showcase
-                  key={showCase.alt + index}
-                  index={index}
-                  activeImage={activeImageIndex}
-                  setActiveImage={setActiveImageIndex}
-                  content={content}
-                />
-              ))}
-  
-              <ThemedP className="mb-8 hidden h-[72px] sm:block">
-                {content[activeImageIndex].paragraph}
-              </ThemedP>
-            </div>
+        <div className="flex sm:flex-row h-[100vh] items-center gap-8 w-full max-w-6xl flex-col">
+          <div className="w-full relative z-10 select-none sm:self-center sm:block flex self-end justify-center ml:mb-8 gap-x-3 md:gap-x-0">
+            {content.map((showCase, index) => (
+              <Showcase
+                key={showCase.alt + index}
+                index={index}
+                activeImage={activeImageIndex}
+                setActiveImage={setActiveImageIndex}
+                content={content}
+              />
+            ))}
 
-            <div
-              className={`justify-self-end z-10 grid items-center justify-center w-full transition-all relative md:w-full h-full sm:h-[450px] outline-offset-4 rounded-2xl overflow-hidden outline-2 outline-light-primary dark:outline-dark-primary
-                  `}
-            >
-              {content.map((image, index) => (
-                <Image
-                  key={image.alt}
-                  src={image.src}
-                  alt={image.alt}
-                  width={700}
-                  height={700}
-                  className={`${
-                    image.fileType === "svg"
-                      ? "object-contain bg-dark-white20 p-8"
-                      : "object-cover object-left-top"
-                  } absolute duration-500  transition-all rounded-[4px] w-full h-full ${
-                    index === activeImageIndex ? "opacity-100" : "opacity-0"
-                  } `}
-                />
-              ))}
-            </div>
-            <ThemedP className="max-h-[48px] mb-8 block self-start sm:hidden">
+            <ThemedP className="mb-8 mt-24 hidden h-[72px] sm:block">
               {content[activeImageIndex].paragraph}
             </ThemedP>
           </div>
+
+          <div
+            className={`justify-self-end z-10 grid items-center justify-center w-full transition-all 
+                relative md:w-full h-full sm:h-[450px] outline-offset-4 rounded-2xl overflow-hidden outline-2 
+                outline-light-primary dark:outline-dark-primary
+                  `}
+          >
+            {content.map((image, index) => (
+              <Image
+                key={image.alt}
+                src={image.src}
+                alt={image.alt}
+                width={700}
+                height={700}
+                className={`${
+                  image.fileType === "svg"
+                    ? "object-contain bg-dark-white20 p-8"
+                    : "object-cover object-left-top"
+                } absolute duration-500 transition-all rounded-[4px] w-full h-full ${
+                  index === activeImageIndex ? "opacity-100" : "opacity-0"
+                } `}
+              />
+            ))}
+          </div>
+          <ThemedP className="max-h-[48px] mb-8 block self-start sm:hidden">
+            {content[activeImageIndex].paragraph}
+          </ThemedP>
+        </div>
       </section>
     </>
   );
@@ -85,27 +87,29 @@ export const Showcase = ({ content, setActiveImage, activeImage, index }) => {
     return null;
   } else {
     return (
-      <ThemedH2 className="">
-        <span
-          onMouseEnter={() => setActiveImage(index)}
-          className={`cursor-pointer group sm:block flex sm:flex-row flex-col items-center align-middle
+      <div className="w-fit">
+        <ShowCaseHeaderText>
+          <span
+            onMouseEnter={() => setActiveImage(index)}
+            className={`cursor-pointer font-bold group sm:block h-[60px] flex flex-col items-center align-middle
             text-light-primary
             dark:text-dark-primary`}
-        >
-          <span
-            className={`
-              group-hover:text-light-primary dark:group-hover:text-dark-primary group-hover:opacity-100 transition-all duration-500 opacity-60 h-fit h1hidden ${
-                activeImage === index
-                  ? "text-dark-primary"
-                  : "text-light-white20 dark:text-dark-black50"
-              }`}
           >
-            <br />
-            Web
+            <span
+              className={`group-hover:text-light-primary dark:group-hover:text-dark-primary 
+                group-hover:opacity-100 transition-all duration-500 opacity-60 h1hidden ${
+                  activeImage === index
+                    ? "text-dark-primary"
+                    : "text-light-white20 dark:text-dark-black50"
+                }`}
+            >
+              <br />
+              Web
+            </span>
+            {content[index].type}
           </span>
-          {content[index].type}
-        </span>
-      </ThemedH2>
+        </ShowCaseHeaderText>
+      </div>
     );
   }
 };
