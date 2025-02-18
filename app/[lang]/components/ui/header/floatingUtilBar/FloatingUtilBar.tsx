71263ec581fee5bd58c462c8e5639ac03c463f12
Joakim <villokodehode@gmail.com>
2025-02-18 20:16:19 +0100

    tweaked utilbar

--- app/[lang]/components/ui/header/floatingUtilBar/FloatingUtilBar.tsx
+++ app/[lang]/components/ui/header/floatingUtilBar/FloatingUtilBar.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { FcSettings } from "react-icons/fc";
 import { useEffect, useState } from "react";
@@ -27,32 +27,35 @@ export const FloatingUtilsBar = () => {
   }, [setNotTop]);
 
   return (
-    // <div className="relative">
+    <div
+      className={`fixed duration-1000  z-[999] transition-all ${
+        showToolBar ? "-translate-x-40" : ""
+      }`}
+    >
       <div
-        className={`fixed transition-all p-3 group top-2 duration-1000 z-[999] hover:bg-opacity-100 border-b-2 hover:border-opacity-100 bg-opacity-40 flex items-center rounded-r-[30px] pr-5 justify-between ${
+        className={`transition-all p-3 group top-2 border-b-2 flex items-center rounded-r-[30px] pr-5 justify-between ${
           notTop ? "animate-Tools-slide-in" : "animate-Tools-slide-out"
         } ${
           theme === "light"
             ? "bg-light-cloud  text-light-ash border-light-violet"
-            : "bg-dark-slate text-dark-frost border-dark-onyx"
+            : "bg-dark-onyx text-dark-frost border-dark-lavender"
         } 
-          ${showToolBar ? "" : "translate-x-[-100%]"}
           `}
       >
         <div
-          className={`flex 2xl:flex-row flex-col opacity-30 group-hover:opacity-100  right-0 transition-all top-0 gap-4`}
+          className={`flex 2xl:flex-row flex-col right-0 transition-all top-0 gap-4`}
         >
           <LanguageSwitcher className={`2xl:flex-row flex-col`} />
 
           <ThemeSwitch />
-          <button onClick={toggleToolBar} className="">
+          <button onClick={toggleToolBar}>
             <FcSettings
-              className={`absolute h-[50px] w-fit top-0 2xl:left-[140px] left-[50px] transition-all duration-500 hover:animate-cogSpin 
-          ${showToolBar ? "" : "2xl:left-[165px] left-[65px]"}`}
+              className={`absolute h-[50px] w-fit top-0 2xl:left-[140px] left-[50px] transition-all duration-500 hover:animate-cog-spin 
+          ${showToolBar ? "2xl:left-[165px] left-[65px]" : ""}`}
             ></FcSettings>
           </button>
         </div>
       </div>
-    // </div>
+    </div>
   );
 };
